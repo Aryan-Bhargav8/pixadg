39c6ac6d570031e4cc63624c3c08849f1a4106f8
Aryan-Bhargav8 <bhargav.aryan8@gmail.com>
2024-09-27 02:28:17 +0530

    added app theme

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,42 @@ class Pixadg extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'PixAdg: Images from Pixabay',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        brightness: Brightness.light
-      ),
+        primarySwatch: MaterialColor(
+          0xFFD7CCC8, // Beige color
+          <int, Color>{
+            50: Color(0xFFEFEBE9),
+            100: Color(0xFFD7CCC8),
+            200: Color(0xFFBCAAA4),
+            300: Color(0xFFA1887F),
+            400: Color(0xFF8D6E63),
+            500: Color(0xFF795548),
+            600: Color(0xFF6D4C41),
+            700: Color(0xFF5D4037),
+            800: Color(0xFF4E342E),
+            900: Color(0xFF3E2723),
+          },
+        ),
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: Color(0xFFF5F5DC), 
+        ),
       darkTheme: ThemeData(
-        brightness: Brightness.dark
+        brightness: Brightness.dark,
+        primarySwatch: MaterialColor(
+          0xFFD7CCC8, // Beige color
+          <int, Color>{
+            50: Color(0xFFEFEBE9),
+            100: Color(0xFFD7CCC8),
+            200: Color(0xFFBCAAA4),
+            300: Color(0xFFA1887F),
+            400: Color(0xFF8D6E63),
+            500: Color(0xFF795548),
+            600: Color(0xFF6D4C41),
+            700: Color(0xFF5D4037),
+            800: Color(0xFF4E342E),
+            900: Color(0xFF3E2723),
+          },
+        ),
+        scaffoldBackgroundColor: Color.fromARGB(255, 74, 48, 44),
       ),
       themeMode: ThemeMode.system,
       home: HomeScreen(),
